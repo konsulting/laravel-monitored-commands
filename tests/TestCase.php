adc793327624cd0b9f2c9044cb597f4ffb619e39
Joco <joco@klever.co.uk>
2023-05-16 11:09:57 +0100

    bumped phpunit and testbench for laravel 10

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,7 +12,7 @@ class TestCase extends OrchestraTestCase
      *
      * @return void
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
